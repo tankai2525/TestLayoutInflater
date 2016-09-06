f301a97be8f94e0da1595125a06f33495da9b04b
luobodidi <1067629251@qq.com>
2016-09-06 17:58:41 +0800

    升级gradle2.1.3

--- app/src/main/java/kai/tan/com/testlayoutinflater/MainActivity.java
+++ app/src/main/java/kai/tan/com/testlayoutinflater/MainActivity.java
@@ -1,13 +1,21 @@
 package kai.tan.com.testlayoutinflater;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LayoutInflater mLayoutInflate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+                setContentView(R.layout.activity_main);
+        mLayoutInflate = LayoutInflater.from(this);
+        View view = mLayoutInflate.inflate(R.layout.activity_main, null);
+        setContentView(view);
     }
 }
