7fab279dd8282dcedfafdfe9b0d98c7c73a5bde0
luobodidi <1067629251@qq.com>
2016-09-08 18:16:59 +0800

    修改

--- app/src/main/java/kai/tan/com/testlayoutinflater/MainActivity.java
+++ app/src/main/java/kai/tan/com/testlayoutinflater/MainActivity.java
@@ -4,18 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private LayoutInflater mLayoutInflate;
+    private LinearLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-                setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        main = (LinearLayout)findViewById(R.id.main);
         mLayoutInflate = LayoutInflater.from(this);
-        View view = mLayoutInflate.inflate(R.layout.activity_main, null);
-        setContentView(view);
+        View button = mLayoutInflate.inflate(R.layout.button, null);
+        main.addView(button);
     }
 }
